fe8c8e5f73e496d35192ecb14356a993ac43cda2
Nathan Monte <nathan.monte@givingforce.com>
2023-01-19 21:58:27 +0000

    introduction of memoized selectors.

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,29 @@
-export const selectCategoryMap = (entireState) => {
-    console.log("Selector fired");
-    return entireState.categories.categoriesArray.reduce((accumulator, category) => {
-        const {title, items} = category;
+import { createSelector } from "reselect";
 
-        accumulator[title.toLowerCase()] = items;
-        return accumulator;
-    }, {});
-}
+// Create selector memoizes selectors to prevent re-renderings when a value hasn't changed.
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    // Input selector - what data do we want to select from?
+    [selectCategoryReducer],
+    // Output selector - If this is different then re-run the method using strict equality.
+    (categoriesSlice) => {
+        console.log("Selector 1");
+        return categoriesSlice.categoriesArray
+    }
+)
+
+export const selectCategoryMap = createSelector(
+    [selectCategories],
+    // If the categoriesArray has not changed give me the previous calculated value.
+    (categories) => {
+        console.log("Selector 2");
+        return categories.reduce((accumulator, category) => {
+            const {title, items} = category;
+
+            accumulator[title.toLowerCase()] = items;
+            return accumulator;
+        }, {});
+    }
+) 
--- src/store/store.js
+++ src/store/store.js
@@ -6,9 +6,31 @@ import { rootReducer } from "./root-reducer";
 // Store just needs the root reducer
 // Logger helps us see the inner working of the store
 
+/**
+ * Currying a function which returns another function to have a variable within a closure.
+ */
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    // Sometimes we don't receive a type as we didn't provide it.
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log("type", action.type);
+    console.log("payload", action.payload);
+    console.log("currentState", store.getState());
+
+    // How does the store get the new state.
+    // Once it's been updated from all of the reducers.
+    // This is working synchronously, but redux logger is not working synchronously 
+    next(action);
+
+    console.log("next state: ", store.getState());
+};
+
 // Middleware is called before the reducer is called
 // Middleware is an enhancer
-const middleware = [logger];
+const middleware = [loggerMiddleware];
 
 // Passes all of the enhancers
 // Compose passes multiple functions left to right
